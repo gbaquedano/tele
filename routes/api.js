7d1f67f0c3e43a3b11c31272a95ac88afc704825
Gabriel <g.baquedano.89@gmail.com>
2016-06-22 19:57:01 +0200

    Guardado de ficheros

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,9 +9,18 @@ router.get('/set/:horaini/:horafin', function(req, res, next){
 });
 
 router.post('/set/:horaini', function(req, res, next){
-	console.log(req.params);
-	console.log(req);
+//	console.log(req.params);
+//	console.log(req);
+	console.log(req.params.horaini);
+	console.log(req.body.datos);
 	res.sendStatus(200);
+	fs.writeFile("datos/" + req.params.horaini + ".csv", req.body.datos, function(err){
+		if(err == null){
+			console.log("Fichero " + req.params.horaini + " guardado OK");
+		}else{
+			console.log("Error al guardar fichero " + req.params.horaini + ":" + err);
+		}
+	});
 });
 
 router.get('/', function(req, res, next) {
