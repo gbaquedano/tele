7a27c2628d97fc3284eae233e8bef14dde043f52
Gabriel <g.baquedano.89@gmail.com>
2016-07-06 21:01:16 +0200

    Añadir diccionario y lista para elegir sensores

--- public/javascripts/telemetria.js
+++ public/javascripts/telemetria.js
@@ -1,10 +1,26 @@
 var telemetria = angular.module('telemetria',[]);
 
-telemetria.controller('graficarCtrl', function($scope) {
-  $scope.greeting = 'Hola!';
-  setInterval(function(){
-   $scope.$apply(function(){
-	   $scope.greeting = $scope.greeting + " a";
-   });
-  }, 1000);
+telemetria.controller('graficarCtrl', function($scope, $http) {
+	$scope.greeting = 'Hola!';
+	$scope.graficados = [];
+	$http.get('/javascripts/diccionario.json').then(function(res){
+		$scope.diccionario = res.data;
+		$scope.diccionarioObj = [];
+		var keys = Object.keys($scope.diccionario);
+		for(var i = 0; i < keys.length; i++){
+			console.log($scope.diccionario[keys[i]]);
+			var values = Object.keys($scope.diccionario[keys[i]]);
+			for(var j = 0; j < values.length; j++){
+				$scope.diccionarioObj.push({ id_trama:keys[i], offset: values[j] , nombre: $scope.diccionario[keys[i]][values[j]] });
+			}
+		}
+		console.log($scope.diccionarioObj);
+	});
+	$scope.addSensors = function(sensores){
+		console.log(sensores);
+		for(var i = 0; i < sensores.length; i++){
+			$scope.graficados.push(sensores[i]);
+		}
+		// Añadir refrescado de gráfico
+	}
 });
