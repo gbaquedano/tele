f0ca51211142df9d14b2ce80100006f2162ce901
Igb <ignaciobfp@gmail.com>
2016-07-08 18:30:24 +0200

    Fechas legibles por humanos en URL. Otras /unixtim

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var moment=require('moment');
 
-/* GET home page. */
+/* //GET home page. 
 router.get('/:inicio/:fin/:grupo/:sensor', function(req, res, next) {
   res.render('index', { title: 'Telemetría', inicio: req.params.inicio, fin: req.params.fin, sensor: req.params.sensor, grupo: req.params.grupo });
+});
+*/
+
+router.get('/unixtimes/:inicio/:fin/', function(req, res, next) {
+  res.render('index', { title: 'Telemetría', inicio: req.params.inicio, fin: req.params.fin});
+});
+
+router.get('/:inicio/:fin/', function(req, res, next) {
+  console.log(moment(req.params.inicio, "DD-MM-YYYY-HH-mm-ss").milliseconds());
+  res.render('index', { title: 'Telemetría', inicio: moment(req.params.inicio, "DD-MM-YYYY-HH-mm-ss").milliseconds(), fin: req.params.fin});
 });
 
 module.exports = router;
